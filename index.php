ce606490089936ab4c98e3e0a91c15742ffff17a
Monjurulhasan <monjurul.hasan98@gmail.com>
2018-04-10 08:39:41 +0600

    PHPcomments

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+// There are two kinds of comments in php
+echo "<h3>This is single line comments</h3>";
+# 1. Single line comments...
+echo "// This is single line comment";
+echo "<br>";
+echo "Or";
+echo "<br>";
+echo "# This is also single line comment";
+echo "<br>";
+echo "<br>";
+echo "<h3>This is Multiline comments</h3>";
+# 2. Multiline comments...
+echo "/* this is Multiline ";
+echo "<br>";
+echo "comment symbol";
+echo "*/";
